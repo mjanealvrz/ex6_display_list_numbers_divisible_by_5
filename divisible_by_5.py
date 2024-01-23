4d9396e74a47fd165c100ab38ff094d9eb558514
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 17:39:57 +0800

    Convert the list into string and removed bracked using list slicing

--- divisible_by_5.py
+++ divisible_by_5.py
@@ -6,6 +6,6 @@
 given_list =  [10, 20, 33, 46, 55]
 
 # Print the given list
-print("Given List is ", given_list)
+print("Given List is ", str(given_list)[1:-1])
 # Print divisible by 5
 # Create a for loop to determine the number in list that is divisible by 5
