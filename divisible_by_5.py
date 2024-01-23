5d174a859dc9fde4c3567f6f89dc4684ddb83078
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 17:51:02 +0800

    Created for loop function

--- divisible_by_5.py
+++ divisible_by_5.py
@@ -7,5 +7,8 @@ given_list =  [10, 20, 33, 46, 55]
 
 # Print the given list
 print("Given List is ", str(given_list)[1:-1])
-# Print divisible by 5
+
 # Create a for loop to determine the number in list that is divisible by 5
+for number in given_list:
+    if  number % 5 == 0:
+        print ("Numbers divisible by 5 are ", number)
