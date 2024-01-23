1bdb545f2869415b71b88245d71ecf4d1ecd367f
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 17:30:53 +0800

    Created pseudocode

--- divisible_by_5.py
+++ divisible_by_5.py
@@ -1,2 +1,8 @@
 # Iterate the given list of numbers and print only those numbers which are divisible by 5
 
+# Pseudocode
+
+# Paste the given list
+# Print the given list
+# Print divisible by 5
+# Create a for loop to determine the number in list that is divisible by 5
