13bb2fae32efae4aef4c62d4a83e90be9e687046
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 17:33:28 +0800

    Pasted and added print to the given list

--- divisible_by_5.py
+++ divisible_by_5.py
@@ -3,6 +3,9 @@
 # Pseudocode
 
 # Paste the given list
+given_list =  [10, 20, 33, 46, 55]
+
 # Print the given list
+print("Given List is ", given_list)
 # Print divisible by 5
 # Create a for loop to determine the number in list that is divisible by 5
